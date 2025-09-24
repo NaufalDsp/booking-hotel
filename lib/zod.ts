2c5630971263a1b96a7c9fae32230599f7f230e1
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-09-24 18:23:52 +0700

    add zod

---
+++ lib/zod.ts
@@ -0,0 +1,8 @@
+import { object, string } from "zod";
+
+export const ContactSchema = object({
+  name: string().min(6, "Name must be at least 6 characters long"),
+  email: string().email("Invalid email address"),
+  subject: string().min(5, "Subject must be at least 5 characters long"),
+  message: string().min(10, "Message must be at least 10 characters long"),
+});
