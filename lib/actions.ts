e46cd530e0c3c81314d7a78d7615b18aa8f27386
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-09-26 20:11:45 +0700

    add validation contact form

---
+++ lib/actions.ts
@@ -0,0 +1,29 @@
+"user server";
+
+import { ContactSchema } from "@/lib/zod";
+import { prisma } from "@/lib/prisma";
+
+export const ContactMessage = async (formData: FormData) => {
+  const validatedFields = ContactSchema.safeParse(
+    Object.fromEntries(formData.entries())
+  );
+  if (!validatedFields.success) {
+    return { errors: validatedFields.error.flatten().fieldErrors };
+  }
+
+  const { name, email, subject, message } = validatedFields.data;
+
+  try {
+    await prisma.contact.create({
+      data: {
+        name,
+        email,
+        subject,
+        message,
+      },
+    });
+    return { message: "Message sent successfully" };
+  } catch (error) {
+    console.log(error);
+  }
+};
--- lib/zod.ts
+++ lib/zod.ts
@@ -2,7 +2,11 @@ import { object, string } from "zod";
 
 export const ContactSchema = object({
   name: string().min(6, "Name must be at least 6 characters long"),
-  email: string().email("Invalid email address"),
+  email: string()
+    .min(6, "Email at least 6 characters")
+    .email("Invalid email address"),
   subject: string().min(5, "Subject must be at least 5 characters long"),
-  message: string().min(10, "Message must be at least 10 characters long"),
+  message: string()
+    .min(10, "Message must be at least 10 characters long")
+    .max(300, "Message must be at most 300 characters long"),
 });
