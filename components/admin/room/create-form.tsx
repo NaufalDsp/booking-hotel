78c8b022c9d9e3d8f603b9a37f36fa7c3b3f0ea3
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-10-19 23:09:22 +0700

    add create form

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -1,7 +1,15 @@
 const CreateForm = () => {
   return (
     <form action="">
-      <div className="grid md:grid-cols-12 gap-5"></div>
+      <div className="grid md:grid-cols-12 gap-5">
+        <div className="col-span-8 bg-white p-4">
+          <div className="mb-4">
+            <input type="text" name="name" className="py-2 px-4 rounded-sm border border-gray-400 w-full"/>
+            <div className=""></div>
+          </div>
+        </div>
+        <div className="col-span-4 bg-white p-4"></div>
+      </div>
     </form>
   );
 };
