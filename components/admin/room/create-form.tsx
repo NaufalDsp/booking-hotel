067667727604004769e635dbad4233f96ba5ad39
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-10-21 21:12:44 +0700

    add create room form in page

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -1,3 +1,5 @@
+import { IoCloudUploadOutline } from "react-icons/io5";
+
 const CreateForm = () => {
   return (
     <form action="">
@@ -46,13 +48,43 @@ const CreateForm = () => {
           >
             <div className="flex flex-col items-center justify-center text-gray-500 pt-5 pb-6 z-10">
               <div className="flex flex-col items-center justify-center">
+                <IoCloudUploadOutline className="size-8" />
                 <p className="mb-1 text-sm font-bold">Select Image</p>
                 <p className="text-xs">
                   SVG, PNG, JPG, GIF, or Others (max: 4MB)
                 </p>
               </div>
             </div>
+            <input type="file" id="input-file" className="hidden" />
           </label>
+          <div className="mb-4">
+            <input
+              type="text"
+              name="capacity"
+              className="py-2 px-4 rounded-sm border border-gray-400 w-full"
+              placeholder="Capacity..."
+            />
+            <div aria-live="polite" aria-atomic="true">
+              <span className="text-sm text-red-500 mt-2">message</span>
+            </div>
+          </div>
+          <div className="mb-4">
+            <input
+              type="text"
+              name="price"
+              className="py-2 px-4 rounded-sm border border-gray-400 w-full"
+              placeholder="Price..."
+            />
+            <div aria-live="polite" aria-atomic="true">
+              <span className="text-sm text-red-500 mt-2">message</span>
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="bg-orange-400 text-white w-full hover:bg-orange-500 py-2.5 px-6 md:px-10 text-lg font-semibold cursor-pointer"
+          >
+            Save
+          </button>
         </div>
       </div>
     </form>
