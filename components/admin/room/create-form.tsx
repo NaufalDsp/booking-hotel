8977baed0815db424ce4a30d08aecef9bc66a12d
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-10-21 20:55:48 +0700

    fix page

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -4,34 +4,52 @@ const CreateForm = () => {
       <div className="grid md:grid-cols-12 gap-5">
         <div className="col-span-8 bg-white p-4">
           <div className="mb-4">
-            <input type="text" name="name" className="py-2 px-4 rounded-sm border border-gray-400 w-full" placeholder="Room Name..."/>
+            <input
+              type="text"
+              name="name"
+              className="py-2 px-4 rounded-sm border border-gray-400 w-full"
+              placeholder="Room Name..."
+            />
             <div aria-live="polite" aria-atomic="true">
               <span className="text-sm text-red-500 mt-2">message</span>
             </div>
           </div>
           <div className="mb-4">
-            <textarea name="description" rows={8} className="py-2 px-4 rounded-sm border border-gray-400 w-full" placeholder="Description"></textarea>
+            <textarea
+              name="description"
+              rows={8}
+              className="py-2 px-4 rounded-sm border border-gray-400 w-full"
+              placeholder="Description"
+            ></textarea>
             <div aria-live="polite" aria-atomic="true">
               <span className="text-sm text-red-500 mt-2">message</span>
             </div>
           </div>
           <div className="mb-4 grid md:grid-cols-3">
-            <input type="checkbox" name="amenities" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"/>
+            <input
+              type="checkbox"
+              name="amenities"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
+            />
             <label className="ms-2 text-sm font-medium text-gray-900 capitalize">
               Wi-Fi
-              </label>
+            </label>
             <div aria-live="polite" aria-atomic="true">
               <span className="text-sm text-red-500 mt-2">message</span>
             </div>
           </div>
         </div>
         <div className="col-span-4 bg-white p-4">
-          <label htmlFor="input-file" className="flex flex-col mb-4 items-center justify-center aspect-video border-2 border-gray-300 border-dashed rounded-md cursor-pointer bg-gray-50 relative">
+          <label
+            htmlFor="input-file"
+            className="flex flex-col mb-4 items-center justify-center aspect-video border-2 border-gray-300 border-dashed rounded-md cursor-pointer bg-gray-50 relative"
+          >
             <div className="flex flex-col items-center justify-center text-gray-500 pt-5 pb-6 z-10">
               <div className="flex flex-col items-center justify-center">
-                
                 <p className="mb-1 text-sm font-bold">Select Image</p>
-                <p className="text-xs">SVG, PNG, JPG, GIF, or Others (max: 4MB)</p>
+                <p className="text-xs">
+                  SVG, PNG, JPG, GIF, or Others (max: 4MB)
+                </p>
               </div>
             </div>
           </label>
