dd28a49753aca588ba70a4dd6cceab96396e8a89
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2025-11-24 20:41:23 +0700

    refactor: format CreateRoom component for improved readability

--- components/admin/room/create-room.tsx
+++ components/admin/room/create-room.tsx
@@ -1,12 +1,15 @@
 import CreateForm from "@/components/admin/room/create-form";
 
-const CreateRoom = () => {
-  return (
-    <div>
-      <h1 className="text-3xl font-bold text-gray-800 mb-4">Create New Room</h1>
-      <CreateForm />
-    </div>
-  );
-};
+const CreateRoom =
+  () => {
+    return (
+      <div>
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          Create New Room
+        </h1>
+        <CreateForm />
+      </div>
+    );
+  };
 
 export default CreateRoom;
