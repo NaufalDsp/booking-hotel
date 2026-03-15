c5728d69c2f7df9967f0367c4fae734fed53b0a5
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-03-15 20:21:46 +0700

    fix: adjust

--- app/admin/room/create/page.tsx
+++ app/admin/room/create/page.tsx
@@ -2,7 +2,7 @@ import CreateRoom from "@/components/admin/room/create-room";
 
 const CreateRoomPage = () => {
   return (
-    <div className="max-w-screen-xl px-4 py-16 mt-10 mx-auto">
+    <div className="max-w-screen-xl px-4 py-8 mt-2 mx-auto">
       <CreateRoom />
     </div>
   );
