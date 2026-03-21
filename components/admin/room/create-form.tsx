0acb703f60066f01b658920af0b118644cae389e
NaufalDsp <145748689+NaufalDsp@users.noreply.github.com>
2026-03-21 19:01:25 +0700

    fix: update image upload UI to conditionally display delete button and upload prompt

--- components/admin/room/create-form.tsx
+++ components/admin/room/create-form.tsx
@@ -91,24 +91,29 @@ const CreateForm = () => {
             htmlFor="input-file"
             className="flex flex-col mb-4 items-center justify-center aspect-video border-2 border-gray-300 border-dashed rounded-md cursor-pointer bg-gray-50 relative">
             <div className="flex flex-col items-center justify-center text-gray-500 pt-5 pb-6 z-10">
-              <div className="flex flex-col items-center justify-center">
-                {pending ? <BarLoader /> : null}
-                <button
-                  type="button"
-                  onClick={() => deleteImage(image)}
-                  className="flex items-center justify-center bg-transparent size-6 rounded-sm absolute right-1 top-1 text-white hover:bg-red-400">
-                  <IoTrashOutline className="size-4 text-transparent hover:text-white" />
-                </button>
-                <IoCloudUploadOutline className="size-8" />
-                <p className="mb-1 text-sm font-bold">Select Image</p>
-                {message ? (
-                  <p className="text-xs text-red-500">{message}</p>
-                ) : (
-                  <p className="text-xs">
-                    SVG, PNG, JPG, GIF, or Others (max: 4MB)
-                  </p>
-                )}
-              </div>
+              {pending ? <BarLoader /> : null}
+              {image ? (
+                <div className="flex flex-col items-center justify-center">
+                  <button
+                    type="button"
+                    onClick={() => deleteImage(image)}
+                    className="flex items-center justify-center bg-transparent size-6 rounded-sm absolute right-1 top-1 text-white hover:bg-red-400">
+                    <IoTrashOutline className="size-4 text-transparent hover:text-white" />
+                  </button>
+                </div>
+              ) : (
+                <div className="flex flex-col items-center justify-center">
+                  <IoCloudUploadOutline className="size-8" />
+                  <p className="mb-1 text-sm font-bold">Select Image</p>
+                  {message ? (
+                    <p className="text-xs text-red-500">{message}</p>
+                  ) : (
+                    <p className="text-xs">
+                      SVG, PNG, JPG, GIF, or Others (max: 4MB)
+                    </p>
+                  )}
+                </div>
+              )}
             </div>
             {!image ? (
               <input
